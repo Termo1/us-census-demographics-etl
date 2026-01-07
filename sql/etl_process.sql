b48094677c136831e59ef93544c5e81ab5563810
Termo1 <adamburansky9@gmail.com>
2026-01-07 16:31:28 +0100

    Add DIM_STATE dimension table

--- sql/etl_process.sql
+++ sql/etl_process.sql
@@ -61,3 +61,72 @@ FROM US_OPEN_CENSUS_DATA__NEIGHBORHOOD_INSIGHTS__FREE_DATASET.PUBLIC."2019_METAD
 CREATE OR REPLACE TABLE stg_geo AS
 SELECT *
 FROM US_OPEN_CENSUS_DATA__NEIGHBORHOOD_INSIGHTS__FREE_DATASET.PUBLIC."2019_METADATA_CBG_GEOGRAPHIC_DATA";
+
+-- ============================================
+-- TRANSFORM PHASE: Dimension Tables
+-- ============================================
+
+-- Dimenzia: DIM_STATE (SCD Type 0)
+CREATE OR REPLACE TABLE dim_state AS
+SELECT
+    ROW_NUMBER() OVER (ORDER BY STATE_FIPS) AS state_id,
+    STATE_FIPS AS state_fips,
+    STATE AS state_abbr,
+    CASE STATE
+        WHEN 'AL' THEN 'Alabama'
+        WHEN 'AK' THEN 'Alaska'
+        WHEN 'AZ' THEN 'Arizona'
+        WHEN 'AR' THEN 'Arkansas'
+        WHEN 'CA' THEN 'California'
+        WHEN 'CO' THEN 'Colorado'
+        WHEN 'CT' THEN 'Connecticut'
+        WHEN 'DE' THEN 'Delaware'
+        WHEN 'DC' THEN 'District of Columbia'
+        WHEN 'FL' THEN 'Florida'
+        WHEN 'GA' THEN 'Georgia'
+        WHEN 'HI' THEN 'Hawaii'
+        WHEN 'ID' THEN 'Idaho'
+        WHEN 'IL' THEN 'Illinois'
+        WHEN 'IN' THEN 'Indiana'
+        WHEN 'IA' THEN 'Iowa'
+        WHEN 'KS' THEN 'Kansas'
+        WHEN 'KY' THEN 'Kentucky'
+        WHEN 'LA' THEN 'Louisiana'
+        WHEN 'ME' THEN 'Maine'
+        WHEN 'MD' THEN 'Maryland'
+        WHEN 'MA' THEN 'Massachusetts'
+        WHEN 'MI' THEN 'Michigan'
+        WHEN 'MN' THEN 'Minnesota'
+        WHEN 'MS' THEN 'Mississippi'
+        WHEN 'MO' THEN 'Missouri'
+        WHEN 'MT' THEN 'Montana'
+        WHEN 'NE' THEN 'Nebraska'
+        WHEN 'NV' THEN 'Nevada'
+        WHEN 'NH' THEN 'New Hampshire'
+        WHEN 'NJ' THEN 'New Jersey'
+        WHEN 'NM' THEN 'New Mexico'
+        WHEN 'NY' THEN 'New York'
+        WHEN 'NC' THEN 'North Carolina'
+        WHEN 'ND' THEN 'North Dakota'
+        WHEN 'OH' THEN 'Ohio'
+        WHEN 'OK' THEN 'Oklahoma'
+        WHEN 'OR' THEN 'Oregon'
+        WHEN 'PA' THEN 'Pennsylvania'
+        WHEN 'RI' THEN 'Rhode Island'
+        WHEN 'SC' THEN 'South Carolina'
+        WHEN 'SD' THEN 'South Dakota'
+        WHEN 'TN' THEN 'Tennessee'
+        WHEN 'TX' THEN 'Texas'
+        WHEN 'UT' THEN 'Utah'
+        WHEN 'VT' THEN 'Vermont'
+        WHEN 'VA' THEN 'Virginia'
+        WHEN 'WA' THEN 'Washington'
+        WHEN 'WV' THEN 'West Virginia'
+        WHEN 'WI' THEN 'Wisconsin'
+        WHEN 'WY' THEN 'Wyoming'
+        WHEN 'PR' THEN 'Puerto Rico'
+        ELSE STATE
+    END AS state_name
+FROM stg_fips
+GROUP BY STATE_FIPS, STATE
+ORDER BY STATE_FIPS;
